adcfbca15760be905871b82cfd25842db488f1ad
iti5606021420131 <5606021420131@fitm.kmutnb.ac.th>
2016-02-14 15:57:53 +0700

    Update Ptice.java

--- oop01/src/Ptice.java
+++ oop01/src/Ptice.java
@@ -1,38 +1,39 @@
 import java.io.*;
 import javax.swing.JOptionPane;
 import java.util.*;
-public class Ptice {
-
-	public static void main(String[] args) 
+public class Ptice 
+{
+	Scanner gold = new Scanner(System.in);
+	int round;
+	int num1=0,num2=0,num3=0;
+	String a;
+	
+	
+	public String getData()
 	{
-		Scanner gold = new Scanner(System.in);
-		int round,num1=0,num2=0,num3=0;
-		String Adrian,Bruno,Goran,answer="";
-		Adrian="ABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABC";
-		Bruno="BABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABC";
-		Goran="CCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABB";
 		
-		System.out.printf("Enter Round : ");
-		round = gold.nextInt();
+		round = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter Round : ","Input",JOptionPane.QUESTION_MESSAGE));
+		
 		while(round <=0 || round >100)
 		{
-			System.out.println("Error Round");
-			System.out.printf("Enter Round : ");
-			round = gold.nextInt();
+			JOptionPane.showMessageDialog(null,"Error Round","Message",JOptionPane.ERROR_MESSAGE);
+			round = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter Round : ","Input",JOptionPane.QUESTION_MESSAGE));
 		}
 		System.out.println();
 
-		System.out.printf("Enter Answer : ");
-		answer = gold.next();
-		while(answer.length() != round)
+		a = JOptionPane.showInputDialog(null,"Enter Answer : ","Input",JOptionPane.QUESTION_MESSAGE);
+
+		while(a.length() != round)
 		{
-			System.out.println("Error Answer");
-			System.out.printf("Enter Answer : ");
-			answer = gold.next();
+			JOptionPane.showMessageDialog(null,"Error Answer","Message",JOptionPane.ERROR_MESSAGE);
+			a = JOptionPane.showInputDialog(null,"Enter Answer : ","Input",JOptionPane.QUESTION_MESSAGE);
 		}
 		System.out.println();
-		
-		
+		return (a);
+	}
+	
+	public void Calc(String Adrian,String Bruno , String Goran,String answer)
+	{
 		for(int i=0;i<round;i++)
 		{
 			if(answer.charAt(i) == Adrian.charAt(i))
@@ -48,60 +49,68 @@ public class Ptice {
 				num3 = num3 + 1;
 			}
 		}
-		
-		
-		
+	}
+	
+	public void Print()
+	{
 		if(num1 == num2)
 		{
 			if(num1 == num3)
 			{
-				System.out.println(num1);
-				System.out.println("Adrian");
-				System.out.println("Bruno");
-				System.out.println("Goran");
+				JOptionPane.showMessageDialog(null,num1+"\n"+"Adrian\n"+"Bruno\n"+"Goran","Message",JOptionPane.INFORMATION_MESSAGE);
 			}
 			else
 			{
-				System.out.println(num1);
-				System.out.println("Adrian");
-				System.out.println("Bruno");
+				JOptionPane.showMessageDialog(null,num1+"\n"+"Adrian\n"+"Bruno\n","Message",JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
 		else if(num2 == num3)
 		{
-			System.out.println(num2);
-			System.out.println("Bruno");
-			System.out.println("Goran");
+			JOptionPane.showMessageDialog(null,num2+"\n"+"Bruno\n"+"Goran","Message",JOptionPane.INFORMATION_MESSAGE);
 		}
 		else if(num1 == num3)
 		{
-			System.out.println(num1);
-			System.out.println("Adrian");
-			System.out.println("Goran");
+			JOptionPane.showMessageDialog(null,num1+"\n"+"Adrian\n"+"Goran","Message",JOptionPane.INFORMATION_MESSAGE);
 		}
 		else if(num1 > num2)
 		{
 			if(num1 > num3)
 			{
-				System.out.println(num1);
-				System.out.println("Adrian");
+				JOptionPane.showMessageDialog(null,num1+"\n"+"Adrian","Message",JOptionPane.INFORMATION_MESSAGE);
 			}
 			else
 			{
-				System.out.println(num3);
-				System.out.println("Goran");
+				JOptionPane.showMessageDialog(null,num3+"\n"+"Goran","Message",JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
 		else if(num2 > num3)
 		{
-			System.out.println(num2);
-			System.out.println("Bruno");
+			JOptionPane.showMessageDialog(null,num2+"\n"+"Bruno","Message",JOptionPane.INFORMATION_MESSAGE);
 		}
 		else 
 		{
+			JOptionPane.showMessageDialog(null,num3+"\n"+"Goran","Message",JOptionPane.INFORMATION_MESSAGE);
 			System.out.println(num3);
 			System.out.println("Goran");
 		}
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		Scanner gold = new Scanner(System.in);
+		String Adrian,Bruno,Goran;	
+		String answer;
+		Adrian="ABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABC";
+		Bruno="BABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABCBABC";
+		Goran="CCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABBCCAABB";
+		
+		
+		Ptice PT = new Ptice();
+		answer = PT.getData();
+		PT.Calc(Adrian,Bruno,Goran,answer);
+		PT.Print();
+			
 	}
 
 }
